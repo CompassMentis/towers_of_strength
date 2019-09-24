8be521a42d57db0e3f0171d2fa816d33ff48d05e
Coen de Groot <cdegroot@nomensa.com>
2019-09-24 22:20:16 +0100

    Added animation test script

--- pygame_test.py
+++ pygame_test.py
@@ -1,8 +1,18 @@
 # Testing out pyGame's SCALED property
 import pygame
+from spritesheet import SpriteSheet, Origin
+import time
 
-# canvas = pygame.display.set_mode((1024, 768), flags=pygame.NOFRAME)
 canvas = pygame.display.set_mode((1020, 700))
+
+# Test animation
+sprites = SpriteSheet('images/runners/heroine_animated.png', columns=32, rows=8, colour_key=pygame.Color(127, 127, 127))
+
+for i in range(32 * 8):
+    canvas.fill((255, 255, 255))
+    sprites.blit(canvas, i, position=(50, 50), origin=Origin.TopLeft)
+    pygame.display.flip()
+    time.sleep(0.2)
 
 while True:
     pass
