322c9e71ad7706a134bbbfdd533d9d46517bfc73
Coen de Groot <cdegroot@nomensa.com>
2019-09-24 19:00:00 +0100

    Decreased image sizes, increased tiles on the grid, added screen image for scaling elements

---
+++ pygame_test.py
@@ -0,0 +1,8 @@
+# Testing out pyGame's SCALED property
+import pygame
+
+# canvas = pygame.display.set_mode((1024, 768), flags=pygame.NOFRAME)
+canvas = pygame.display.set_mode((1020, 700))
+
+while True:
+    pass
--- settings.py
+++ settings.py
@@ -1,17 +1,19 @@
 class Settings:
     # To do: calculate these number
-    tile_size = (100, 100)
-    cell_width = 88
-    cell_height = 88
+    scaling_factor = 0.7
+
+    tile_size = (int(100 * scaling_factor), int(100 * scaling_factor))
+    cell_width = int(88 * scaling_factor)
+    cell_height = cell_width
 
     # To do: improve this
     canvas_width = 1000
-    canvas_height = 800
+    canvas_height = 700
 
-    tiles_offset_x = 200
-    tiles_offset_y = 300
+    tiles_offset_x = 90
+    tiles_offset_y = 310
 
-    runner_size = (69, 83)
+    runner_size = (int(69 * scaling_factor), int(83 * scaling_factor))
 
-    tower_bystander_size = (70, 70)
-    tower_marshall_size = (70, 70)
+    tower_bystander_size = (int(70 * scaling_factor), int(70 * scaling_factor))
+    tower_marshall_size = tower_bystander_size
